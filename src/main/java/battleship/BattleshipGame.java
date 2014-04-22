689904fb25ca4c1d12c692b65b80631479cafc92
nburma01 <nburma01@dcs.bbk.ac.uk>
2014-04-22 20:21:51 +0100

    Added code to main class, new code playAgain.

--- src/main/java/battleship/BattleshipGame.java
+++ src/main/java/battleship/BattleshipGame.java
@@ -14,36 +14,41 @@ public class BattleshipGame {
         // Welcome message.
         System.out.println("Welcome to the Battleship Game!\n");
 
-        // Set up the ocean.
-        Ocean ocean = new Ocean();
-
-        // Place the ships.
-        ocean.placeAllShipsRandomly();
-
-        // Loop to accept shots until game over.
+        // Repeat for each game.
         do {
-            // Fire a shot. Report if hit or miss.
-            if (shoot(ocean)) {
-                System.out.println("Congratulations! You got a hit!");
-            } else {
-                System.out.println("Sorry, you missed.");
-            }
+            // Set up the ocean.
+            Ocean ocean = new Ocean();
 
-            // Display the ocean.
-            System.out.println(ocean.toString());
+            // Place the ships.
+            ocean.placeAllShipsRandomly();
 
-        } while (!ocean.isGameOver());
+            // Loop to accept shots until game over.
+            do {
+                // Fire a shot. Report if hit or miss.
+                if (shoot(ocean)) {
+                    System.out.println("Congratulations! You got a hit!");
+                } else {
+                    System.out.println("Sorry, you missed.");
+                }
 
-        // Print final scores.
-        System.out.println("Game over!\n"
-            + "----------\n"
-            + "Shots fired : " + ocean.getShotsFired() + "\n"
-            + "Hit count   : " + ocean.getHitCount() + "\n"
-            + "Ships sunk  : " + ocean.getShipsSunk());
+                // Display the ocean.
+                System.out.println(ocean.toString());
 
-        //TODO Display where the ships were (nice to have)
+            } while (!ocean.isGameOver());
 
-        //TODO ask if the user wants to play again
+            // Print final scores.
+            System.out.println("Game over!\n"
+                    + "----------\n"
+                    + "Shots fired : " + ocean.getShotsFired() + "\n"
+                    + "Hit count   : " + ocean.getHitCount() + "\n"
+                    + "Ships sunk  : " + ocean.getShipsSunk());
+
+            //TODO Display where the ships were (nice to have)
+
+        } while (playAgain());
+
+        // Sign off message to user.
+        System.out.println("\nThank you for playing. Goodbye.");
     }
 
     /**
@@ -52,20 +57,48 @@ public class BattleshipGame {
      * @param ocean The ocean being shot at.
      * @return      If there was a hit.
      */
-    private static boolean shoot(Ocean ocean){
+    private static boolean shoot(Ocean ocean) {
 
         // Open an input channel.
-        Scanner input = new Scanner(System.in);
+        Scanner inputShoot = new Scanner(System.in);
 
         // Get the row to fire at.
         System.out.print("Row: ");
-        int row = input.nextInt();
+        int row = inputShoot.nextInt();
 
         // Get the column to fire at.
         System.out.print("Column: ");
-        int column = input.nextInt();
+        int column = inputShoot.nextInt();
 
         // Shoot and return hit status.
         return ocean.shotAt(row, column);
+    }
+
+    /**
+     * Ask the user if they want to play again.
+     *
+     * @return True if they want to play again.
+     */
+    private static boolean playAgain() {
+
+        // Open an input channel.
+        Scanner inputPa = new Scanner(System.in);
+
+        // Prompt user.
+        System.out.print("Play again (Y/N) default=N ? : ");
+
+        // Get input.
+        String sPa = inputPa.nextLine();
+
+        // If any user input.
+        if (sPa.length() > 0) {
+            // Check first character of user response.
+            if (Character.toUpperCase(sPa.charAt(0)) == 'Y') {
+                return true;
+            }
+        }
+
+        // If user does not want to.
+        return false;
     }
 }
